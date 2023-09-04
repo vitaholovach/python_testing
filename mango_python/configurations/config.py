27c8f5478c968cf1825e638571acb81fe1eb7adc
Vita <vitapronina15@hmail.com>
2023-09-04 14:23:26 +0300

    hw 22

--- mango_python/configurations/config.py
+++ mango_python/configurations/config.py
@@ -1,13 +1,13 @@
 [client_data]
-client_url = mongodb://localhost:27017
+client_url = mongodb: // localhost: 27017
 
 [collections_data]
 my_brands = [{"brand": "Rabbithole", "price": 125, "quantity": 10},
-           {"brand": "DeLav", "price": 226, "quantity": 14},
-           {"brand": "Natureal", "price": 320, "quantity": 16},
-           {"brand": "Sue", "price": 741, "quantity": 26},
-           {"brand": "Sane", "price": 562, "quantity": 19},
-           {"brand": "Clarins", "price": 100, "quantity": 22}]
+             {"brand": "DeLav", "price": 226, "quantity": 14},
+             {"brand": "Natureal", "price": 320, "quantity": 16},
+             {"brand": "Sue", "price": 741, "quantity": 26},
+             {"brand": "Sane", "price": 562, "quantity": 19},
+             {"brand": "Clarins", "price": 100, "quantity": 22}]
 
 my_products = [{"brand": "Rabbithole", "type": "pomade", "color": "red"},
                {"brand": "DeLav", "type": "pomade", "color": "pink"},
