03f1733d3d6685ae4d24a2c524416f430695c12f
Vita <vitapronina15@hmail.com>
2023-09-04 14:20:29 +0300

    hw 22

--- mango_python/sqlall/main.py
+++ mango_python/sqlall/main.py
@@ -7,16 +7,12 @@ coll_brands, coll_products = CollectionBrands(), CollectionProducts()
 if __name__ == "__main__":
     coll_brands.insert_many(ReadConfig.get_brands_collection_document())
     coll_brands.find_all()
-
     coll_products.insert_many(ReadConfig.get_products_collection_document())
     coll_products.find_all()
-
     coll_products.find_one("brand", "Rabbithole")
     coll_brands.find_one_no_id("brand", "Clarins")
-
     coll_brands.delete_one("brand", "Clarins")
     coll_brands.find_all()
-
     coll_brands.delete_all()
     coll_brands.find_all()
     coll_products.find_all()
